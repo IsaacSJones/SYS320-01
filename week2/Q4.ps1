4fdf4f2496ee6979a1b64831a45e743e4a164178
IsaacSJones <79865738+IsaacSJones@users.noreply.github.com>
2024-01-29 14:56:17 -0500

    Add files via upload

---
+++ week2/Q4.ps1
@@ -0,0 +1,13 @@
+﻿clear
+
+#Q4
+$chrome = Get-Process -Name "chrome" -ErrorAction SilentlyContinue
+$website = "https://www.champlain.edu"
+
+if ($chrome) {
+    Write-Host "closing chrome"
+    Stop-Process -Name "chrome"
+} else {
+    Write-Host "opening chrome: routing to" $website
+    Start-Process "chrome" -ArgumentList $website
+}
