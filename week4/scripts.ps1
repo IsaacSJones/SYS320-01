f10f574383b59acf46d2c4bab541ad6565465f2f
IsaacSJones <79865738+IsaacSJones@users.noreply.github.com>
2024-02-14 15:07:48 -0500

    Add files via upload

---
+++ week4/scripts.ps1
@@ -0,0 +1,35 @@
+﻿clear
+
+#Q3
+#Get-Content C:\xampp\apache\logs\access.log
+
+#Q4
+#Get-Content C:\xampp\apache\logs\access.log -Tail 5
+
+#Q5
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 ', ' 400 '
+
+#Q6
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 200 ' -NotMatch
+
+#Q7
+#$A = Get-ChildItem -Path C:\xampp\apache\logs\*.log | Select-String 'error'
+#$A[0..-4]
+
+#Q8
+#$notfounds = Get-Content -Path C:\xampp\apache\logs\access.log | Select-String ' 404 '
+    
+#$regex = [regex] "\b(?:\d{1,3}\.){3}\d{1,3}\b"
+
+#$ipsUnorganized = $regex.Matches($notfounds)
+
+#$ips = @()
+#for ($i=0; $i -lt $ipsUnorganized.Count; $i++) {
+#    $ips += [PSCustomObject]@{ "IP" = $ipsUnorganized[$i].value; }
+#}
+#$ips | Where-Object { $_.IP -ilike "192.*" }
+
+#Q9
+#$ipsoftens = $ips | Where-Object { $_.IP -ilike "*" }
+#$counts = $ipsoftens | Group-Object IP
+#$counts | Select-Object Count, Name
