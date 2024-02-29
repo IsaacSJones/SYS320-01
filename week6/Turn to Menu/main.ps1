a9da17aaefefa250c355ea07714db86a352d4747
IsaacSJones <79865738+IsaacSJones@users.noreply.github.com>
2024-02-29 13:03:02 -0500

    Add files via upload

---
+++ week6/Turn to Menu/main.ps1
@@ -0,0 +1,72 @@
+﻿. (Join-Path $PSScriptRoot Apache-Parse.ps1)
+. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot work.ps1)
+
+clear
+
+$Prompt  = "Please choose your operation:`n"
+$Prompt += "1  - List last 10 apache logs`n"
+$Prompt += "2  - List last 10 failed logins`n"
+$Prompt += "3  - List at risk users`n"
+$Prompt += "4  - Launch champlain.edu`n"
+$Prompt += "5  - Exit`n"
+
+
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -eq 1) {
+        $tableRecords = ApacheLogs1
+        $tableRecords | Select-Object -Last 10 | Format-Table -AutoSize -Wrap 
+    
+    }
+
+
+    elseif($choice -eq 2){
+
+        $userLogins = getFailedLogins 365
+
+        Write-Host ($userLogins | Select-Object -Last 10 | Format-Table | Out-String)
+    }
+
+
+    elseif($choice -eq 3){
+        
+        $riskLevel = 3
+
+        $days = Read-Host -Prompt "Please enter the number of days to check"
+
+        $userLogins = getRiskUsers $days $riskLevel
+
+        Write-Host ($userLogins | Format-Table | Out-String )
+    }
+
+
+    elseif($choice -eq 4){
+
+        browser
+    }
+
+
+    else {
+        Write-Host "Unrecognized input, please enter an available option." | Out-String
+    }
+
+}
+
+
+
+
---
+++ week6/Turn to Menu/work.ps1
@@ -0,0 +1,27 @@
+﻿clear
+
+#Q1
+#Get-Process | Where-Object { $_.ProcessName -ilike 'C*' }
+
+#Q2
+#Get-Process | Where-Object { $_.Path -inotlike "*system32*" } | Select-Object ProcessName, Path
+
+#Q3
+#$folderpath = "C:\Users\Isaac\SYS320-01\outfolder"
+#$filePath = Join-Path -Path $folderpath "stopped.csv"
+#Get-Service | Where-Object { $_.Status -ilike "Stopped" } | Sort-Object |`
+#Export-Csv -Path $filepath
+
+#Q4 modified for Turn to menu assignment
+function browser(){
+    $chrome = Get-Process -Name "chrome" -ErrorAction SilentlyContinue
+    $website = "https://www.champlain.edu"
+
+    if ($chrome) {
+        Write-Host "closing chrome"
+        Stop-Process -Name "chrome"
+    } else {
+        Write-Host "opening chrome: routing to" $website
+        Start-Process "chrome" -ArgumentList $website
+    }
+}
